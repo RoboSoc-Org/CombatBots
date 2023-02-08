ee93e92e839c8790c65e220f69d6d0e599489d4b
Dan <68428249+OpenElement@users.noreply.github.com>
2023-02-08 13:17:22 +0000

    Update CombatBot_v2040.ino

--- CombatBot_v2040.ino
+++ CombatBot_v2040.ino
@@ -25,7 +25,7 @@
 #define GPIO_2 12   //I2C: SDA_0,   SPI: DI,    UART: TX_0
 #define GPIO_1 13   //I2C: SCL_0,   SPI: CS,    UART: RX_0
 
-int ch1, ch2, ch3, ch4, ch1Map, ch2Map, ch3Map, ch4Map, HPWM, VPWM, RWD, LWD;
+int ch1, ch2, ch3, ch4, ch1Map, ch2Map, ch3Map, ch4Map, HPWM, VPWM, WPWM, TestPWM,RWD, LWD, WPN;
 
 void setup() {
 
@@ -62,10 +62,10 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  ch1 = pulseIn(RC_Ch1, HIGH, 30000); // Read the pulse width of 
-  ch2 = pulseIn(RC_Ch2, HIGH, 30000); // each channel
-  ch3 = pulseIn(RC_Ch3, HIGH, 30000); // Read the pulse width of 
-  ch4 = pulseIn(RC_Ch4, HIGH, 30000); // each channel
+  ch4 = pulseIn(RC_Ch1, HIGH, 30000); // Read the pulse width of
+  ch3 = pulseIn(RC_Ch2, HIGH, 30000); // each channel
+  ch2 = pulseIn(RC_Ch3, HIGH, 30000); // Read the pulse width of 
+  ch1 = pulseIn(RC_Ch4, HIGH, 30000); // each channel
 
   Serial.print("ch1: ");
   Serial.print(ch1);
@@ -79,10 +79,16 @@ void loop() {
 
 
   HPWM = map(ch1, 1000, 2000, 0, 510); // 1000 & 2000, Experimental Pulse Width Values for CombatBotPCB v2040
-  VPWM = map(ch2, 1000, 2000, 0, 510); //   1000 & 2000, Experimental Pulse Width Values for CombatBotPCB v2040
+  VPWM = map(ch2, 1000, 2000, 0, 510); // 1000 & 2000, Experimental Pulse Width Values for CombatBotPCB v2040
 
-  RWD = VPWM - (HPWM - 255);  // Right Wheel Drive: 0-200 backwards, 200-300 Stationary, 300 - 510 forwards
-  LWD = VPWM + (HPWM - 255); //   Left Wheel Drive: 0-200 backwards, 200-300 Stationary, 300 - 510 forwards
+  WPWM = map(ch3,1000,2000,1250,2500);
+  //WPWM = map(ch3, 1000, 2000, 0, 510);
+  //TestPWM = map(ch4,1000,2000,0,510);
+  
+  //WPN = WPWM - (TestPWM - 255);
+  LWD = VPWM - (HPWM - 255);  // Right Wheel Drive: 0-200 backwards, 200-300 Stationary, 300 - 510 forwards
+  RWD = VPWM - (255 - HPWM); //   Left Wheel Drive: 0-200 backwards, 200-300 Stationary, 300 - 510 forwards
+
   
   //-------------------------------//
  //         Left wheel PWM
@@ -105,17 +111,34 @@ void loop() {
   if (RWD >= 300) {                         // Move Fowards
     analogWrite(Motor_2B, 0);
     analogWrite(Motor_2A, int(RWD - 255));
-  } else if (RWD >= 200 && RWD <= 300) { // Stationary, apply brake
+  }else if (RWD >= 200 && RWD <= 300) { // Stationary, apply brake
     analogWrite(Motor_2B, 1);
     analogWrite(Motor_2A, 1);
   } else {                             // Move Backwards
     analogWrite(Motor_2A, 0);
     analogWrite(Motor_2B, int(255 - RWD));
+ }
+
+
+
+digitalWrite(GPIO_1,HIGH);
+delayMicroseconds(WPWM);
+digitalWrite(GPIO_1,LOW);
+delayMicroseconds(20000-WPWM);
+  //-------------------------------//
+ //        Weapon
+//--------------------------------//
+/*
+  if (WPN >= 300) {                         // Move Fowards
+    analogWrite(Motor_3B, 0);
+    analogWrite(Motor_3A, int(WPN - 255));
+  } else if (WPN >= 200 && WPN <= 300) { // Stationary, apply brake
+    analogWrite(Motor_3B, 1);
+    analogWrite(Motor_3A, 1);
   }
-  
-  /*
+}
+
   delay(2000);
-  
   analogWrite(Motor_1A, 0);
   analogWrite(Motor_1B, 254);
   analogWrite(Motor_2A, 1);
@@ -124,7 +147,7 @@ void loop() {
   analogWrite(Motor_3B, 1);
   
   delay(2000);
-  
+ /*
   analogWrite(Motor_1A, 1);
   analogWrite(Motor_1B, 1);
   analogWrite(Motor_2A, 0);
@@ -140,6 +163,6 @@ void loop() {
   analogWrite(Motor_2B, 1);
   analogWrite(Motor_3A, 0);
   analogWrite(Motor_3B, 254);
-  */
   
+  */
 }
